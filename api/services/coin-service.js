b14a358e4f6d38e0e7cb6cddbdc83b7ea4a9e90f
Thiago Resende <thiago.gcresende@gmail.com>
2018-09-08 01:19:00 -0300

    lint

--- api/services/coin-service.js
+++ api/services/coin-service.js
@@ -1,4 +1,4 @@
-const changeTotal = (code, value) => {
+const changeTotal = (code) => {
   return new Promise((resolve, reject) => {
     User.findOne({ code }).then((userFrom) => {
         let total = userFrom.total + 1;
