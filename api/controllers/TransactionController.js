236b6c3980820d9cc103e45bf2eaee27991ae585
Thiago Resende <thiago.gcresende@gmail.com>
2018-09-08 00:12:13 -0300

    keep

--- api/controllers/TransactionController.js
+++ api/controllers/TransactionController.js
@@ -10,10 +10,10 @@ const transactionService = require('../services/transaction-serivce.js');
 module.exports = {
   create: (req, res) => {
     var transaction = req.body;
-    transactionService.transfer(transaction).then((data) => {
+    transactionService.transfer(transaction).then(() => {
       return res.status(200).json({})
     }).catch((err) => {
-      return res.status(500).json(err)
+      return res.status(500).json({})
     });
   },
   update: (req, res) => {
