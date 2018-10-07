6211329bcbb72114a6e0a7611acb385fcb1175b0
Thiago Resende <thiago.gcresende@gmail.com>
2018-10-06 21:05:42 -0300

    icons

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,13 +1,21 @@
 var app = angular.module("app", ['ngRoute']);
 app.config(function ($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-  $locationProvider.hashPrefix('');
   $routeProvider.when('/', {
     templateUrl: './js/app/views/home/home.template.html',
     controller: 'homeController'
-  })
+  });
+  $locationProvider.html5Mode(true);
+  $locationProvider.hashPrefix('');
 });
 
+function startUi() {
+  $(document).ready(function () {
+    $('.sidenav').sidenav();
+  });
+}
+
+
+startUi();
 //cadastro
 //home
 //extrato
