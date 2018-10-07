4663c2e8b02f3c55340886a5d3a6a1196e916531
Thiago Resende <thiago.gcresende@gmail.com>
2018-10-07 00:15:56 -0300

    New User

---
+++ assets/js/app/services/user.service.js
@@ -0,0 +1,18 @@
+app.service("userService",
+    function ($http) {
+    
+        var url = 'api/user/';
+        var request = {};
+        save = function (item) {
+            if (!item.id || item.id == 0)
+                request = $http({ method: 'POST', url: url, data: item });
+            else
+                request = $http({ method: 'PUT', url: url, data: item });
+            return request;
+        }
+
+        return ({
+            save: save,
+        });
+    }
+);
---
+++ assets/js/app/views/new-account/new-account.controller.js
@@ -0,0 +1,3 @@
+app.controller('newAccountController', function ($scope, $rootScope, $location) {
+  $rootScope.showAppBar = false;
+});
