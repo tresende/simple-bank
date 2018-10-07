38637c2fa3e9e621a2cb07babfba1ed657a972b0
Thiago Resende <thiago.gcresende@gmail.com>
2018-10-06 21:47:32 -0300

    avatars

--- assets/js/app/views/home/home.controller.js
+++ assets/js/app/views/home/home.controller.js
@@ -1,3 +1,29 @@
 app.controller('homeController', function ($scope) {
-  $scope.text = 'ok';
+
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {});
+
+  $scope.detail = function (item) {
+    console.log(item);
+    instances[0].open();
+  }
+
+  $scope.data = [{
+      to: 'Camila Rodrigues',
+      value: 100,
+      type: 0,
+      date: new Date(),
+    },
+    {
+      to: 'Camila Rodrigues',
+      value: 30,
+      type: 1,
+      date: new Date(),
+    }, {
+      to: 'Camila Rodrigues',
+      value: 52,
+      type: 1,
+      date: new Date(),
+    }
+  ]
 });
